a01ea3e7a251d69e350d270e84c2025de90abaac
Nathan Perry <nateperry333@gmail.com>
2011-11-22 22:35:29 -0500

    INCOMPLETE

--- Classes/KIFStep-Protocol.h
+++ Classes/KIFStep-Protocol.h
@@ -3,29 +3,21 @@
 
 
 #import <Foundation/Foundation.h>
+#import "KIFStepProtocol.h"
 
 
 @class KIFContext;
-@protocol KIFStepNotifications;
-
-
-/* !
- @typedef  KIFStep
- * /
-typedef NSObject<KIFStepNotifications> KIFStep;
-@class KIFStep;  // Treat KIFStep as a class and color it as such.
-// */
 
 
 #pragma mark -
 
 
-/*!
+/**
  @protocol  KIFTestStepDelegate
  @abstract  Delegate for a step.
  @discussion  This is the step delegate used to execute steps.  Each step has a sequence of functions that are executed in the following order: shouldRunStep, stepWillBegin, runStep and stepWillTerminate.  Any function that returns false (NO) will cancel the remaining function calls except for stepWillTerminate.  stepWillTerminate is always called before the next step is run.
  */
-@protocol KIFStepNotifications <NSObject>
+@protocol KIFStep <NSObject>
 
 
 @optional
@@ -34,7 +26,7 @@ typedef NSObject<KIFStepNotifications> KIFStep;
 #pragma mark Sequence
 
 
-/*!
+/**
  @method  shouldRunStep
  @abstract  Gets a value indicating if this step should be run.
  @discussion  When implemented, used to test if the step should be run; otherwise the step is run.  This is called first.  The step is considered successful even if shouldRunStep returns false (NO).
@@ -42,12 +34,12 @@ typedef NSObject<KIFStepNotifications> KIFStep;
 - (BOOL)shouldRunStep:(KIFContext *)context;
 
 
-/*!
+/**
  */
 - (NSArray *)preSteps;
 
 
-/*!
+/**
  @method  stepWillBegin
  @abstract  Performs any needed step preparation.
  @discussion  When implemented, may be used to perform pre-step 
@@ -55,7 +47,7 @@ typedef NSObject<KIFStepNotifications> KIFStep;
 - (BOOL)stepWillBegin:(KIFContext *)context;
 
 
-/*!
+/**
  @method  runStep
  @abstract  Runs this step.
  @result  Returns a value indicating the success of this step.
@@ -64,12 +56,12 @@ typedef NSObject<KIFStepNotifications> KIFStep;
 - (BOOL)runStep:(KIFContext *)context;
 
 
-/*!
+/**
  */
 - (NSArray *)postSteps;
 
 
-/*!
+/**
  @method  stepDidEnd
  @abstract  Performs any needed step cleanup.
  @discussion  When implemented, may be used to perform post-step cleanup.  Always run even if shouldRunStep
@@ -80,7 +72,7 @@ typedef NSObject<KIFStepNotifications> KIFStep;
 #pragma mark Step descriptors
 
 
-/*!
+/**
  @method  stepDescription
  @abstract  Gets the description of this step.
  @discussion  When implemented and returns a valid value, used in the log message for this step.  Otherwise, the default message will be used.
@@ -88,7 +80,7 @@ typedef NSObject<KIFStepNotifications> KIFStep;
 - (NSString *)stepDescription;
 
 
-/*!
+/**
  @method  stepTimeout
  @abstract  Gets the timeout of this step.
  @discussion  When implemented and returns a valid value, used to define the timeout of this step.  Otherwise, the default timeout is used.
